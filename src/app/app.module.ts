c9e58551a6680fd26bb955dcd9b80c0b59b0fbee
Jonathan C <jchiang@8thlight.com>
2019-05-18 16:43:55 -0500

    add ngsx package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,8 @@ import { HeaderComponent } from './components/layout/header/header.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NoPageComponent } from './components/pages/no-page/no-page.component';
-import { IndexModule } from './modules/index/index.module';
-import { AuthRequiredModule } from './modules/auth-required/auth-required.module';
-import { TodoListComponent } from './components/shared/todo-list/todo-list.component';
-import { TodoListItemComponent } from './components/shared/todo-list-item/todo-list-item.component';
 import { AddTodoComponent } from './components/shared/add-todo/add-todo.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LogoutComponent } from './components/pages/logout/logout.component';
 import { NotAuthorizedComponent } from './components/pages/not-authorized/not-authorized.component';
 
 @NgModule({
